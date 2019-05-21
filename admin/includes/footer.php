d4e1541c2439ca47db7770cb71b0fb823087b4ab
NaqibRifqi <doraboy96@gmail.com>
2019-05-21 13:37:11 +0800

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Library Management System |<a href="http://vle.unikl.edu.my/" target="_blank"  > Designed by : HANANANI Sdn. Bhd</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
