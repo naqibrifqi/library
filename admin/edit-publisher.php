b09769f3566d02a322d3776699cf1e623f7f810b
NaqibRifqi <doraboy96@gmail.com>
2019-05-27 23:10:52 +0800

    Update edit-publisher.php

--- admin/edit-publisher.php
+++ admin/edit-publisher.php
@@ -49,7 +49,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                 <div class="container">
                     <div class="row pad-botm">
                         <div class="col-md-12">
-                            <p><a href="dashboard.php">Dashboard</a> > <a href="manage-categories.php">Manage publisher</a> > <a href="javascript:window.location.href=window.location.href">Edit Categories</a></p>
+                            <p><a href="dashboard.php">Dashboard</a> > <a href="manage-categories.php">Manage publisher</a> > <a href="javascript:window.location.href=window.location.href">Edit Publisher</a></p>
                             <h4 class="header-line">Edit Publisher</h4>
 
                         </div>
